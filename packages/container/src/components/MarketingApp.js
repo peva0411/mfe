d06a5a90cf6eecc075c993eb66b466127b14a8e7
Phil Evans <pevans@paylocity.com>
2021-11-29 19:52:28 -0500

    update marketing app again

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -4,7 +4,7 @@ import React, { useRef, useEffect } from 'react';
 export default () => {
   const ref = useRef(null);
 
-  useEffect(() => { 
+  useEffect(() => {  
     mount(ref.current);
   });
 
